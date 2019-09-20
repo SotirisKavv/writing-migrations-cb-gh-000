79bc04ea0bc60790af60745809fc481a99902a80
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-20 14:29:26 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,8 +2,8 @@ class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
     create_table :artist do |t|
-      t.string = :name
-      t.birthdate = :birthdate
+      t.string :name
+      t.birthdate :birthdate
     end
   end
 end
