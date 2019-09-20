6d67f84b0eca9377624bd2ffde0862dc8d196701
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-20 14:34:56 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
+    sql = <<-SQL
+    CREATE TABLE students(
+      name TEXT
+    )
+    SQL
+    
     create_table :artist do |t|
       t.string :name
     end
