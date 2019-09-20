654d2846df5ea337282e602de346493dd2f4aada
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-20 14:47:28 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,13 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-    sql = <<-SQL
-    CREATE TABLE students(
-      name TEXT
-    )
-    SQL
-    ActiveRecord::Base.connection.execute(sql)
-
     create_table :students do |t|
       t.string :name
     end
