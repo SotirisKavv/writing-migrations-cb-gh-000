fd840858eb73f800fa981ff67b695787dd02818a
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-20 14:56:47 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :grade, :birthdate
+    add_column :students, :birthdate, :string
+    add_column :students, :grade, :string
   end
 end
