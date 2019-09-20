955f0c363f35804a95a09d8d74059a169701e979
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-20 15:02:15 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    up
+    remove_column :students, :birthdate, :string
     add_column :students, :birthdate, :datetime
   end
 end
